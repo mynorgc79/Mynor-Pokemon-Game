d33f794f31595e2a7b9ccc06f3ccdfc9b6d22d2d
mynorgc79 <mynorguarchaj99@gmail.com>
2023-08-21 23:04:36 -0600

    add desing button

--- src/components/PokemonOptions.vue
+++ src/components/PokemonOptions.vue
@@ -23,7 +23,10 @@
         <li v-for="pokemon in pokemons" 
         :key="pokemon.id"
         @click="$emit('selection',pokemon.id)"
+        class="pokemon-item"
         >
+   
+        
             {{ pokemon.name }} 
          </li>
         
@@ -53,6 +56,30 @@
         justify-content: center;
 
     }
+
+    .pokemon-item {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 10px;
+  border-radius: 5px;
+  border: 1px solid rgba(0, 0, 0, 0.2);
+  cursor: pointer;
+  margin-bottom: 10px;
+  width: 250px;
+  background: linear-gradient(to right, #a9ac1a, rgba(11, 150, 62, 0.653));/* Fondo predeterminado */
+  color: white; /* Color de texto predeterminado */
+  font-family: 'Montserrat', sans-serif; /* Cambia la fuente aquí */
+  font-size: 16px;
+
+
+}
+.pokemon-item:hover {
+    background: linear-gradient(to right,  gold, gold); /* Fondo al pasar el cursor */
+  color: black; /* Color de texto al pasar el cursor */
+}
+
+
 
 
     
